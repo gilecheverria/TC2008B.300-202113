2bb01336ee79b7ed673a4f6280598cd3dbe5ccd9
Gilberto Echeverria <gilbertoef@gmail.com>
2021-11-05 20:49:01 -0600

    Transfomations in Unity

--- Assets/Scripts/Primitive.cs
+++ Assets/Scripts/Primitive.cs
@@ -4,62 +4,64 @@ using UnityEngine;
 
 public class Primitive : MonoBehaviour
 {
+    [SerializeField] float increment;
+
+    Mesh mesh;
+    Vector3[] verts;
+    int[] faces;
+    Vector3[] modified;
+
+    float angle = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         // New mesh to store the object shape
-        Mesh mesh = new Mesh();
+        mesh = new Mesh();
         // Link to the mesh on the game object
         GetComponent<MeshFilter>().mesh = mesh;
 
         //// GEOMETRY ////
         // The vertices
-        Vector3[] verts = new Vector3[3];
+        verts = new Vector3[3];
         verts[0] = new Vector3(0, 0, 0);
         verts[1] = new Vector3(6, 0, 0);
         verts[2] = new Vector3(3, 6, 0);
 
+        modified = new Vector3[verts.Length];
+
         //// TOPOLOGY ////
-        int[] faces = new int[6];
+        faces = new int[6];
         faces[0] = 0;
         faces[1] = 2;
         faces[2] = 1;
         faces[3] = 0;
         faces[4] = 1;
         faces[5] = 2;
+    }
+
+    void Update()
+    {
+        angle += increment;
 
         //// TRANSFORMS ////
         // Get a translation matrix
-        Matrix4x4 translation = MakeTranslate(-4, 5, 3);
-        Matrix4x4 scale = MakeScale(0.5f, 2.5f, 0.5f);
-        Matrix4x4 composite = translation * scale;
+        //Matrix4x4 translation = Transforms.MakeTranslate(-4, 5, 3);
+        //Matrix4x4 scale = Transforms.MakeScale(0.5f, 2.5f, 0.5f);
+        //Matrix4x4 composite = translation * scale;
+        Matrix4x4 transOrigin = Transforms.MakeTranslate(-3, -6, 0);
+        Matrix4x4 transObject = Transforms.MakeTranslate(3, 6, 0);
+        Matrix4x4 rotate = Transforms.MakeRotateX(angle);
+        //Matrix4x4 composite = transOrigin * rotate * transObject;
+        Matrix4x4 composite = transObject * rotate * transOrigin;
         // Apply the transformation to all vertices
         for (int i=0; i<verts.Length; i++) {
             Vector4 temp = new Vector4(verts[i].x, verts[i].y, verts[i].z, 1);
-            verts[i] = composite * temp;
+            modified[i] = composite * temp;
         }
 
         // Add the model data to the mesh
-        mesh.vertices = verts;
+        mesh.vertices = modified;
         mesh.triangles = faces;
-        
-    }
-
-    Matrix4x4 MakeTranslate(float tx, float ty, float tz)
-    {
-        Matrix4x4 mat = Matrix4x4.identity;
-        mat[0, 3] = tx;
-        mat[1, 3] = ty;
-        mat[2, 3] = tz;
-        return mat;
-    }
-
-    Matrix4x4 MakeScale(float sx, float sy, float sz)
-    {
-        Matrix4x4 mat = Matrix4x4.identity;
-        mat[0, 0] = sx;
-        mat[1, 1] = sy;
-        mat[2, 2] = sz;
-        return mat;
     }
 }
